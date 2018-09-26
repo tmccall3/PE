5b452d3152b3896ac8eeb63eaa482f5d252c23e8
McCall <tmccall@itron.com>
2018-09-26 14:28:27 -0700

    starting to get the hang of it

--- PE8.cpp
+++ PE8.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 int main() {
 	//make a change
+
+	//alright, finally starting to get the hang of this. Although something is still not quite right, I feel.
 	
 	system("pause");
 	return 0;
