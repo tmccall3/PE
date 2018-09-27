4b585d307310ded8829723bde5247e70c6ad09c5
McCall <tmccall@itron.com>
2018-09-27 10:54:24 -0700

    update the PE8, add needed files

--- PE8.cpp
+++ PE8.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
 int main() {
+	
+	ifstream inFile;
+	inFile.open("C:\\Users\\tmccall\\OneDrive - Itron\\Desktop\\Git\\PE\\PE8.txt");
+	string myString;
+
+
+	int count = 0;
+
+	
+
+
+
+	//while (!inFile.eof())
+	//{
+
+
+	//}
 
 	
 	system("pause");
